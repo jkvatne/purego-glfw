a70f5f1e89a5c30304bda5e56e3df33d2214cdc5
Jan Kåre Vatne <jkvatne@online.no>
2026-03-13 10:25:38 +0100

    Improve opacity demo

--- tests/opacity.go
+++ tests/opacity.go
@@ -10,11 +10,11 @@ import (
 	"unsafe"
 
 	glfw "github.com/jkvatne/purego-glfw"
-	gl "github.com/jkvatne/purego-glfw/gl"
+	"github.com/jkvatne/purego-glfw/gl"
 )
 
 func OpacityMain() {
-	fmt.Printf("\nThis windows should gradualy be transparent\n")
+	fmt.Printf("\nThis windows should gradually fade in and out\n")
 	runtime.LockOSThread()
 	// Initialize the glfw library
 	err := glfw.Init()
@@ -26,6 +26,9 @@ func OpacityMain() {
 
 	glfw.WindowHint(glfw.ContextVersionMajor, 2)
 	glfw.WindowHint(glfw.ContextVersionMinor, 0)
+	// Need to make initial window invisible in order to start with opacity=0
+	// If not, the window will show briefly with full intensity.
+	glfw.WindowHint(glfw.Visible, 0)
 	window, err := glfw.CreateWindow(400, 400, "Transparent", nil, nil)
 	if err != nil {
 		glfw.Terminate()
@@ -45,17 +48,17 @@ func OpacityMain() {
 	// Setup vertex shader
 	var vertexShaderSource = `
 		#version 110
-		attribute vec2 vPos;
+		attribute vec2 pos;
 		void main()
 		{
-			gl_Position = vec4(vPos, 0.0, 1.0);	
+			gl_Position = vec4(pos, 0.0, 1.0);	
 		}
 	` + "\x00"
-	vertex_shader := gl.CreateShader(gl.VERTEX_SHADER)
+	vertexShader := gl.CreateShader(gl.VERTEX_SHADER)
 	source, free := gl.Strs(vertexShaderSource)
-	gl.ShaderSource(vertex_shader, 1, source, nil)
+	gl.ShaderSource(vertexShader, 1, source, nil)
 	free()
-	gl.CompileShader(vertex_shader)
+	gl.CompileShader(vertexShader)
 
 	// Setup fragment shader
 	var fragmentShaderSource = `
@@ -65,32 +68,36 @@ func OpacityMain() {
 			gl_FragColor = vec4(1.0);
 		}
 	` + "\x00"
-	fragment_shader := gl.CreateShader(gl.FRAGMENT_SHADER)
+	fragmentShader := gl.CreateShader(gl.FRAGMENT_SHADER)
 	source, free = gl.Strs(fragmentShaderSource)
-	gl.ShaderSource(fragment_shader, 1, source, nil)
+	gl.ShaderSource(fragmentShader, 1, source, nil)
 	free()
-	gl.CompileShader(fragment_shader)
+	gl.CompileShader(fragmentShader)
 
 	// Create program
 	program := gl.CreateProgram()
-	gl.AttachShader(program, vertex_shader)
-	gl.AttachShader(program, fragment_shader)
+	gl.AttachShader(program, vertexShader)
+	gl.AttachShader(program, fragmentShader)
 	gl.LinkProgram(program)
 
 	// Create and bind buffers
-	var vertex_buffer uint32
-	gl.GenBuffers(1, &vertex_buffer)
-	gl.BindBuffer(gl.ARRAY_BUFFER, vertex_buffer)
+	var vertexBuffer uint32
+	gl.GenBuffers(1, &vertexBuffer)
+	gl.BindBuffer(gl.ARRAY_BUFFER, vertexBuffer)
 
 	// Create and setup attribute (vPos)
-	vposLocation := gl.GetAttribLocation(program, gl.Str("vPos\x00"))
-	gl.EnableVertexAttribArray(uint32(vposLocation))
-	gl.VertexAttribPointer(uint32(vposLocation), 2, gl.FLOAT, false, 4, nil)
+	vPosLocation := gl.GetAttribLocation(program, gl.Str("pos\x00"))
+	gl.EnableVertexAttribArray(uint32(vPosLocation))
+	gl.VertexAttribPointer(uint32(vPosLocation), 2, gl.FLOAT, false, 4, nil)
 
 	glfw.SetTime(0)
-	for !window.ShouldClose() && glfw.GetTime() < 4.0 {
+	// Set opacity to 0 before showing the window to make it gradually appear
+	window.SetOpacity(0)
+	window.Show()
+	for !window.ShouldClose() && glfw.GetTime() < 5.0 {
 		t := glfw.GetTime()
-		o := min(t/2, max(0, min(1.0, 2.0-t/2)))
+		// Ramp up over 1.5 seconds, then max in two second and ramp down over 1.5 seconds
+		o := min(t/1.5, (5.0-t)/1.5, 1.0)
 		window.SetOpacity(o)
 
 		// Clear screen to a brown color
@@ -98,7 +105,7 @@ func OpacityMain() {
 		gl.Clear(gl.COLOR_BUFFER_BIT)
 
 		// Draw a triangle
-		var v = [3][2]float32{{-0.7, -0.7}, {0.7, 0.7}, {0.7, -0.7}}
+		var v = [3][2]float32{{0.0, 0.7}, {-0.7, -0.7}, {0.7, -0.7}}
 		gl.UseProgram(program)
 		gl.BufferData(gl.ARRAY_BUFFER, int(unsafe.Sizeof(v)), unsafe.Pointer(&v), gl.STREAM_DRAW)
 		gl.DrawArrays(gl.TRIANGLES, 0, 3)
@@ -110,5 +117,6 @@ func OpacityMain() {
 		time.Sleep(time.Millisecond * 20)
 	}
 	window.Destroy()
+	fmt.Printf("Now the window should have disappeared completely\n")
 	glfw.Terminate()
 }
